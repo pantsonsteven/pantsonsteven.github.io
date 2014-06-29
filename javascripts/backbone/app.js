6e9932762b83d724ade8020a1fef9db1ae8d666e
pantsonsteven <pantsonsteven@gmail.com>
2014-06-29 00:57:19 -0400

    view does not transition up on initial page load or reload. prevents extra real estate from being generate at the bottom of the window

--- javascripts/backbone/app.js
+++ javascripts/backbone/app.js
@@ -1,3 +1,4 @@
+
 // <!-- INITIALIZE -->
 
 var Portfolio = {
@@ -8,71 +9,71 @@ var Portfolio = {
 }
 
 var pages = [
-   {
-      page        : "index",
-      heading     : "index",
-      imageUrl    : "",
-      content1    : "ask me about my knitting",
-      content2    : "",
-      span1       : "",
-      span2       : "",
-      videoMp4    : "",
-      videoOgg    : "",
-      videoWidth  : "",
-      videoHeight : ""
-   },
-   {
-      page        : "peddlr",
-      heading     : "peddlr",
-      imageUrl    : "./images/peddlr.jpg",
-      content1    : "peddlr is an app that helps you plan your Citi Bike trips",
-      content2    : "",
-      span1       : "",
-      span2       : "",
-      videoMp4    : "",
-      videoOgg    : "",
-      videoWidth  : "",
-      videoHeight : ""
-   },
-   {
-      page        : "valence",
-      heading     : "valence",
-      imageUrl    : "./images/valence01.jpg",
-      content1    : "Valence lets you visualize and shape your playlists",
-      content2    : "",
-      span1       : "",
-      span2       : "",
-      videoMp4    : "",
-      videoOgg    : "",
-      videoWidth  : "",
-      videoHeight : ""
-   },
-   {
-      page        : "publications",
-      heading     : "publications",
-      imageUrl    : "",
-      content1    : "Once upon a time I was an academic, here's some of my work",
-      content2    : "",
-      span1       : "",
-      span2       : "",
-      videoMp4    : "",
-      videoOgg    : "",
-      videoWidth  : "",
-      videoHeight : ""
-   },
-   {
-      page        : "about",
-      heading     : "about",
-      imageUrl    : "",
-      content1    : "I like to knit. Thanks for asking. I went to school for a long time and came out a doctor*. But I like making things more than I like thinking quietly in isolation about things so I became a web developer.",
-      content2    : "*Of media...",
-      span1       : "",
-      span2       : "",
-      videoMp4    : "",
-      videoOgg    : "",
-      videoWidth  : "",
-      videoHeight : ""
-   }
+{
+   page        : "index",
+   heading     : "index",
+   imageUrl    : "",
+   content1    : "ask me about my knitting",
+   content2    : "",
+   span1       : "",
+   span2       : "",
+   videoMp4    : "",
+   videoOgg    : "",
+   videoWidth  : "",
+   videoHeight : ""
+},
+{
+   page        : "peddlr",
+   heading     : "peddlr",
+   imageUrl    : "./images/peddlr.jpg",
+   content1    : "peddlr is an app that helps you plan your Citi Bike trips",
+   content2    : "",
+   span1       : "",
+   span2       : "",
+   videoMp4    : "",
+   videoOgg    : "",
+   videoWidth  : "",
+   videoHeight : ""
+},
+{
+   page        : "valence",
+   heading     : "valence",
+   imageUrl    : "./images/valence01.jpg",
+   content1    : "Valence lets you visualize and shape your playlists",
+   content2    : "",
+   span1       : "",
+   span2       : "",
+   videoMp4    : "",
+   videoOgg    : "",
+   videoWidth  : "",
+   videoHeight : ""
+},
+{
+   page        : "publications",
+   heading     : "publications",
+   imageUrl    : "",
+   content1    : "Once upon a time I was an academic, here's some of my work",
+   content2    : "",
+   span1       : "",
+   span2       : "",
+   videoMp4    : "",
+   videoOgg    : "",
+   videoWidth  : "",
+   videoHeight : ""
+},
+{
+   page        : "about",
+   heading     : "about",
+   imageUrl    : "",
+   content1    : "I like to knit. Thanks for asking. I went to school for a long time and came out a doctor*. But I like making things more than I like thinking quietly in isolation about things so I became a web developer.",
+   content2    : "*Of media...",
+   span1       : "",
+   span2       : "",
+   videoMp4    : "",
+   videoOgg    : "",
+   videoWidth  : "",
+   videoHeight : ""
+}
 ]
 
 Portfolio.initialize = function() {
@@ -82,24 +83,30 @@ Portfolio.initialize = function() {
    Backbone.history.start();
 };
 
+// ---------------------------------------------------------------------------
 // <!-- ROUTER -->
 Portfolio.Router = Backbone.Router.extend({
    initialize : function() {
       this.collection = Portfolio.collection;
    },
    routes: {
-   ''             : 'index',
-   'home'         : 'index',
-   'peddlr'       : 'peddlr',
-   'valence'      : 'valence',
-   'publications' : 'publications',
-   'about'        : 'about'
+      ''             : 'index',
+      'home'         : 'home',
+      'peddlr'       : 'peddlr',
+      'valence'      : 'valence',
+      'publications' : 'publications',
+      'about'        : 'about'
    },
    index: function() {
       var page             = this.collection.where({page: 'index'})[0];
       var indexView        = new Portfolio.Views.PageView({ model : page });
       Portfolio.viewManager.display(indexView);
    },
+   home: function() {
+      var page             = this.collection.where({page: 'index'})[0];
+      var homeView        = new Portfolio.Views.PageView({ model : page });
+      Portfolio.viewManager.display(homeView);
+   },   
    peddlr: function() {
       var page             = this.collection.where({page: 'peddlr'})[0];
       var peddlrView       = new Portfolio.Views.PageView({ model : page });
@@ -123,6 +130,7 @@ Portfolio.Router = Backbone.Router.extend({
 });
 
 
+// ---------------------------------------------------------------------------
 // <!-- MODEL -->
 Portfolio.Models.Page = Backbone.Model.extend({
    defaults: {
@@ -141,52 +149,48 @@ Portfolio.Models.Page = Backbone.Model.extend({
 });
 
 
+// ---------------------------------------------------------------------------
 // <!-- COLLECTION -->
 Portfolio.Collections.PageCollection = Backbone.Collection.extend({
    model : Portfolio.Models.Page
 });
 
 
+// ---------------------------------------------------------------------------
 // <!-- VIEW MANAGER -->
 Portfolio.Views.ViewManager = Backbone.View.extend({
-
    display : function(view) {
-
       var previousView = this.currentView || null;
       var nextView     = view;
 
-      nextView.render({ page: true }).$el.appendTo(this.$el);
 
       if (previousView){
-
+         nextView.render({ page: true }).$el.appendTo(this.$el);
          nextView.transitionIn(function() {
             previousView.remove();
          });
-         this.currentView = nextView;
-
       } else {
-
-         nextView.transitionIn();
-         this.currentView = nextView; 
-
+         nextView.render({ page: true })
+            .$el
+            .css({
+               "-webkit-transform"  : "translate3d(0, 0, 0)",
+               "transform"          : "translate3d(0, 0, 0)"
+            })
+            .hide()
+            .appendTo(this.$el)
+            .fadeIn(); 
       };
-
-
+      this.currentView = nextView;
    }
 });
 
 
-
-
+// ---------------------------------------------------------------------------
 // <!-- VIEW -->
 Portfolio.Views.PageView = Backbone.View.extend({
-   
    template : _.template($('.page-template').html()),
-
    render   : function(options) {
-
       options = options || {};
-
       var html = (this.template(this.model.attributes));
       this.$el.html(html);
       this.$el.addClass(this.model.attributes.page+' section')
@@ -198,30 +202,20 @@ Portfolio.Views.PageView = Backbone.View.extend({
       $('.page-wrapper').append(this.$el);
       return this;
    },
-
    transitionIn: function(callback) {
-
       var that = this;
-
       var animateIn = function() {
-         
          that.$el.addClass('is-visible');
-
          that.$el.one('transitionend', function () {
-
-            if (_.isFunction(callback)) {
-               callback();
-            }
+            callback(); 
          });
       };
-
       _.delay(animateIn, 20);
-
    }
-
 })
 
 
+// ---------------------------------------------------------------------------
 // <!-- ON LOAD -->
 $(function() {
    Portfolio.initialize();
