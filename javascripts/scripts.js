60984f71013fcb11be7c339cd55aa0178adf41ca
pantsonsteven <pantsonsteven@gmail.com>
2014-07-10 10:52:12 -0400

    added border pointers for navigation

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,25 +1,25 @@
 
 
 $(function() {
-   // var body = $('body, html');
-   $('#').click(function() {
-      console.log('yay')
-      $(window).scrollTop($('.fifth').offset().top);
+   var body = $('body, html');
+   $('.home').click(function() {
+      var top = $('.fifth').offset().top;
+      body.animate({scrollTop: top}, 750, 'linear');
    })
 
-   $('#about').click(function() {
+   $('.about').click(function() {
       $(window).scrollTop($('.second').offset().top);
    })
 
-   $('#work').click(function() {
+   $('.work').click(function() {
       $(window).scrollTop($('.third').offset().top);
    })
 
-   $('#resume').click(function() {
+   $('.resume').click(function() {
       $(window).scrollTop($('.fourth').offset().top);
    })   
 
-   $('#contact').click(function() {
+   $('.contact').click(function() {
       $(window).scrollTop($('.fifth').offset().top);
    })
 })
