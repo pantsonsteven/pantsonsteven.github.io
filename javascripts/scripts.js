58e362ae1c1e8af9c176e9505ce3981caae2ec78
pantsonsteven <pantsonsteven@gmail.com>
2014-07-29 14:40:41 -0400

    adds javascript to make sections visible

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -12,10 +12,46 @@ function menuToggle(clickedItem) {
   $(clickedItem).addClass('current');
 }
 
+function makeVisible(div) {
+  var visible = $('.visible');
+  visible.addClass('hidden').removeClass('visible');
+  div.addClass('visible').removeClass('hidden');
+}
+
+function menuClick() {
+
+  $('.menu-home').click(function(e) {
+    menuToggle(e.target);
+    var home = $('.home');
+    makeVisible(home);
+  })
+
+  $('.menu-work').click(function(e) {
+    menuToggle(e.target);
+    var work = $('.work');
+    makeVisible(work);
+  })
+
+  $('.menu-resume').click(function(e) {
+    menuToggle(e.target);
+    var resume = $('.resume');
+    makeVisible(resume);
+  })
+
+  $('.menu-about').click(function(e) {
+    menuToggle(e.target);
+    var about = $('.about');
+    makeVisible(about);
+  })
+}
 
 
+function appInitialize() {
+  menuHighlight();
+  menuClick();
+
+}
 
 $(function() {
-  menuHighlight();
-  menuToggle();
+  appInitialize();
 })
